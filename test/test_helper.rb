1a288acbe154491735e4df9128be4e48f8558f64
goncaloelisio <goncaloelisio@gmail.com>
2020-11-26 18:42:42 +0000

    Login added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,13 @@ class ActiveSupport::TestCase
 
   require "minitest/reporters"
   Minitest::Reporters.use!
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
 end
